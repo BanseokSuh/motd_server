7eb81de6c3656d65bd071730bd291524b54709c7
BanseokSuh <still3028@gmail.com>
2024-07-11 07:34:53 +0900

    Feat: Create user-entity

---
+++ src/main/java/com/lightcc/motd/domain/user/infrastructure/entity/UserEntity.java
@@ -0,0 +1,59 @@
+package com.lightcc.motd.domain.user.infrastructure.entity;
+
+import com.lightcc.motd.domain.user.domain.User;
+import com.lightcc.motd.domain.user.domain.UserRole;
+import com.lightcc.motd.global.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Table(name = "\"user\"", uniqueConstraints = {
+        @UniqueConstraint(name = "uk_user_login_id", columnNames = "login_id")
+})
+@Entity
+public class UserEntity extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "login_id", nullable = false, unique = true)
+    private String loginId;
+
+    @Column(name = "user_name", nullable = false)
+    private String userName;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "role", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private UserRole role;
+
+    public static UserEntity from(User user) {
+        UserEntity userEntity = new UserEntity();
+        userEntity.setLoginId(user.getLoginId());
+        userEntity.setUserName(user.getUserName());
+        userEntity.setEmail(user.getEmail());
+        userEntity.setPassword(user.getPassword());
+        userEntity.setRole(user.getRole());
+        return userEntity;
+    }
+
+    public User toDomain() {
+        User user = new User();
+        user.setId(id);
+        user.setLoginId(loginId);
+        user.setUserName(userName);
+        user.setEmail(email);
+        user.setPassword(password);
+        user.setRole(role);
+        return user;
+    }
+}
+
