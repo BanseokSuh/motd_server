c985545c2578ee6e91284478db210cf2181a367b
BanseokSuh <still3028@gmail.com>
2024-07-13 16:46:43 +0900

    Feat: Remove excluding security auto-configuration

--- src/main/java/com/lightcc/motd/MotdApplication.java
+++ src/main/java/com/lightcc/motd/MotdApplication.java
@@ -2,9 +2,8 @@ package com.lightcc.motd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+@SpringBootApplication
 public class MotdApplication {
 
     public static void main(String[] args) {
