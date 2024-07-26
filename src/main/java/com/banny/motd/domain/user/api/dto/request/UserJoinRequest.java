0c00b6ba3de5b4829a4319c25902628e10664c48
BanseokSuh <still3028@gmail.com>
2024-07-26 23:24:02 +0900

    Feat: Add validation for UserJoinRequest

--- src/main/java/com/banny/motd/domain/user/api/dto/request/UserJoinRequest.java
+++ src/main/java/com/banny/motd/domain/user/api/dto/request/UserJoinRequest.java
@@ -1,13 +1,21 @@
 package com.banny.motd.domain.user.api.dto.request;
 
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 
 @Getter
 public class UserJoinRequest {
-    
+
+    @NotBlank(message = "아이디를 입력해주세요")
     private String loginId;
+
+    @NotBlank(message = "이름을 입력해주세요")
     private String userName;
+
+    @NotBlank(message = "비밀번호를 입력해주세요")
     private String password;
+
+    @NotBlank(message = "성별을 선택해주세요")
     private String gender;
 }
