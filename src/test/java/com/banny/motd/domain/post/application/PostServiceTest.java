bf8415795ab800ada8807998a745133464c3b5c7
BanseokSuh <still3028@gmail.com>
2024-08-25 10:55:50 +0900

    Feat: Use h2 database when run test code
    Divide test profile only for run test code

--- src/test/java/com/banny/motd/domain/post/application/PostServiceTest.java
+++ src/test/java/com/banny/motd/domain/post/application/PostServiceTest.java
@@ -13,6 +13,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.util.Optional;
 
@@ -21,6 +22,7 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class PostServiceTest {
 
