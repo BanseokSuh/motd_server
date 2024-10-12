d7ae4ede7c69ca1d2a0bcb44491cb88de1218ce9
banseoksuh <still3028@gmail.com>
2024-10-12 23:06:49 +0900

    Fix: Gender enum 수정

--- src/main/java/com/banny/motd/domain/user/domain/Gender.java
+++ src/main/java/com/banny/motd/domain/user/domain/Gender.java
@@ -1,9 +1,14 @@
 package com.banny.motd.domain.user.domain;
 
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 public enum Gender {
 
-    M,
-    F,
-    ETC
+    MALE("남자"),
+    FEMALE("여자"),
+    ETC("기타");
+
+    private final String text;
 
 }
