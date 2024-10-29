e5d6b7dec43251a858ac2dc30f3e4b57052b240c
banseoksuh <still3028@gmail.com>
2024-10-30 07:25:21 +0900

    Feat: 유저 로그인 성공 케이스, 존재하지 않는 아이디일 경우 실패 케이스 테스트 코드 작성

--- src/test/java/com/banny/motd/api/service/user/UserServiceTest.java
+++ src/test/java/com/banny/motd/api/service/user/UserServiceTest.java
@@ -1,7 +1,9 @@
 package com.banny.motd.api.service.user;
 
 import com.banny.motd.api.service.user.request.UserJoinServiceRequest;
+import com.banny.motd.api.service.user.request.UserLoginServiceRequest;
 import com.banny.motd.domain.user.Gender;
+import com.banny.motd.domain.user.Tokens;
 import com.banny.motd.domain.user.User;
 import com.banny.motd.domain.user.infrastructure.UserRepository;
 import com.banny.motd.global.email.EmailHandler;
@@ -49,14 +51,7 @@ class UserServiceTest {
         String email = "still3028@gmail.com";
         String password = "test001!";
         String gender = "MALE";
-        UserJoinServiceRequest request = UserJoinServiceRequest.builder()
-                .loginId(loginId)
-                .userName(userName)
-                .nickName(nickName)
-                .email(email)
-                .password(password)
-                .gender(gender)
-                .build();
+        UserJoinServiceRequest request = getUserJoinServiceRequest(loginId, userName, nickName, email, password, gender);
 
         // when
         User joinedUser = userService.join(request);
@@ -79,14 +74,7 @@ class UserServiceTest {
         String email = "still3028@gmail.com";
         String password = "test001!";
         String gender = "MALE";
-        UserJoinServiceRequest request = UserJoinServiceRequest.builder()
-                .loginId(loginId)
-                .userName(userName)
-                .nickName(nickName)
-                .email(email)
-                .password(password)
-                .gender(gender)
-                .build();
+        UserJoinServiceRequest request = getUserJoinServiceRequest(loginId, userName, nickName, email, password, gender);
         userService.join(request); // 1회 회원가입
 
         // when // then
@@ -110,14 +98,7 @@ class UserServiceTest {
         String email = "still3028@gmail.com";
         String password = "test001!";
         String gender = "WRONG_GENDER";
-        UserJoinServiceRequest request = UserJoinServiceRequest.builder()
-                .loginId(loginId)
-                .userName(userName)
-                .nickName(nickName)
-                .email(email)
-                .password(password)
-                .gender(gender)
-                .build();
+        UserJoinServiceRequest request = getUserJoinServiceRequest(loginId, userName, nickName, email, password, gender);
 
         // when // then
         assertThatThrownBy(() -> userService.join(request))
@@ -130,11 +111,48 @@ class UserServiceTest {
                 );
     }
 
+    @DisplayName("로그인이 정상적으로 동작한다.")
     @Test
     void login() {
         // given
+        String loginId = "test000";
+        String userName = "서반석";
+        String nickName = "반석";
+        String email = "still3028@gmail.com";
+        String password = "test001!";
+        String gender = "MALE";
+        String device = "WEB";
+        UserJoinServiceRequest joinRequest = getUserJoinServiceRequest(loginId, userName, nickName, email, password, gender);
+        UserLoginServiceRequest loginRequest = getUserLoginServiceRequest(loginId, password, device);
+        userService.join(joinRequest); // 회원가입
+
         // when
+        Tokens token = userService.login(loginRequest);
+
         // then
+        assertThat(token)
+                .extracting("accessToken", "refreshToken")
+                .isNotNull();
+    }
+
+    @DisplayName("로그인 시 존재하지 않는 로그인 아이디로 요청이 들어오면 예외가 발생한다.")
+    @Test
+    void loginWithNotExistLoginId() {
+        // given
+        String loginId = "test000";
+        String password = "test001!";
+        String device = "WEB";
+        UserLoginServiceRequest loginRequest = getUserLoginServiceRequest(loginId, password, device);
+
+        // when // then
+        assertThatThrownBy(() -> userService.login(loginRequest))
+                .isInstanceOf(ApplicationException.class)
+                .extracting("status.code", "status.desc", "result.message")
+                .contains(
+                        ApiResponseStatusType.FAIL_USER_NOT_FOUND.getCode(),
+                        ApiResponseStatusType.FAIL_USER_NOT_FOUND.getDesc(),
+                        String.format("User %s is not found", loginId)
+                );
     }
 
     @Test
@@ -157,4 +175,24 @@ class UserServiceTest {
         // when
         // then
     }
+
+    private static UserLoginServiceRequest getUserLoginServiceRequest(String loginId, String password, String device) {
+        return UserLoginServiceRequest.builder()
+                .loginId(loginId)
+                .password(password)
+                .device(device)
+                .build();
+    }
+
+    private static UserJoinServiceRequest getUserJoinServiceRequest(String loginId, String userName, String nickName, String email, String password, String gender) {
+        return UserJoinServiceRequest.builder()
+                .loginId(loginId)
+                .userName(userName)
+                .nickName(nickName)
+                .email(email)
+                .password(password)
+                .gender(gender)
+                .build();
+    }
+
 }
